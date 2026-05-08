6b6e912534ef35c03d7433fa2e90351737b4b38d
WadaHirono <donchan.hirono528@gmail.com>
2026-05-09 00:20:31 +0900

    fix contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,31 +1,37 @@
-<a
-  href="/演奏依頼書.xlsx"
-  download
-  style={{
-    display: "block",
-    padding: "14px",
-    background: "#222",
-    color: "#fff",
-    borderRadius: "8px",
-    textDecoration: "none",
-    textAlign: "center",
-  }}
->
-  🎼 演奏依頼書をダウンロード
-</a>
+<div style={{ display: "flex", flexDirection: "column", gap: "16px", marginBottom: "30px" }}>
 
-<a
-  href="/レッスン申込書.xlsx"
-  download
-  style={{
-    display: "block",
-    padding: "14px",
-    background: "#444",
-    color: "#fff",
-    borderRadius: "8px",
-    textDecoration: "none",
-    textAlign: "center",
-  }}
->
-  🎤 レッスン申込書をダウンロード
-</a>
+  {/* 演奏依頼 */}
+  <a
+    href="/演奏依頼書.xlsx"
+    download
+    style={{
+      display: "block",
+      padding: "14px 20px",
+      background: "#111",
+      color: "#fff",
+      textAlign: "center",
+      borderRadius: "8px",
+      textDecoration: "none",
+    }}
+  >
+    🎼 演奏依頼書をダウンロード
+  </a>
+
+  {/* レッスン申込 */}
+  <a
+    href="/レッスン申込書.xlsx"
+    download
+    style={{
+      display: "block",
+      padding: "14px 20px",
+      background: "#444",
+      color: "#fff",
+      textAlign: "center",
+      borderRadius: "8px",
+      textDecoration: "none",
+    }}
+  >
+    🎤 レッスン申込書をダウンロード
+  </a>
+
+</div>
