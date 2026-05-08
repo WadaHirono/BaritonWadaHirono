64b6e04008ce65391bc8df0ce56aa065538c97ec
WadaHirono <donchan.hirono528@gmail.com>
2026-05-09 00:35:52 +0900

    fix contact page properly

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,13 +1,22 @@
 export default function ContactPage() {
   return (
-    <div style={{ display: "flex", flexDirection: "column", gap: "16px" }}>
-      <a href="/演奏依頼書.xlsx" download>
-        🎼 演奏依頼書をダウンロード
-      </a>
+    <main style={{ padding: "40px" }}>
+      <h1>お問い合わせ</h1>
 
-      <a href="/レッスン申込書.xlsx" download>
-        🎤 レッスン申込書をダウンロード
-      </a>
-    </div>
+      <div style={{ display: "flex", flexDirection: "column", gap: "16px", marginTop: "20px" }}>
+        <a href="/演奏依頼書.xlsx" download>
+          🎼 演奏依頼書をダウンロード
+        </a>
+
+        <a href="/レッスン申込書.xlsx" download>
+          🎤 レッスン申込書をダウンロード
+        </a>
+      </div>
+
+      <p style={{ marginTop: "30px" }}>
+        その他のお問い合わせはメールでご連絡ください。<br />
+        📩 example@gmail.com
+      </p>
+    </main>
   );
 }
