f308dbb43e9526367f20aa5cc48825faafea5bd7
WadaHirono <donchan.hirono528@gmail.com>
2026-05-09 00:28:44 +0900

    fix JSX error

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,36 +1,10 @@
-<div style={{ display: "flex", flexDirection: "column", gap: "16px", marginBottom: "30px" }}>
+<div style={{ display: "flex", flexDirection: "column", gap: "16px" }}>
 
-  {/* 演奏依頼 */}
-  <a
-    href="/演奏依頼書.xlsx"
-    download
-    style={{
-      display: "block",
-      padding: "14px 20px",
-      background: "#111",
-      color: "#fff",
-      textAlign: "center",
-      borderRadius: "8px",
-      textDecoration: "none",
-    }}
-  >
+  <a href="/演奏依頼書.xlsx" download>
     🎼 演奏依頼書をダウンロード
   </a>
 
-  {/* レッスン申込 */}
-  <a
-    href="/レッスン申込書.xlsx"
-    download
-    style={{
-      display: "block",
-      padding: "14px 20px",
-      background: "#444",
-      color: "#fff",
-      textAlign: "center",
-      borderRadius: "8px",
-      textDecoration: "none",
-    }}
-  >
+  <a href="/レッスン申込書.xlsx" download>
     🎤 レッスン申込書をダウンロード
   </a>
 
