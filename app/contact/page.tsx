2e51b07adfbc92c24b1f3064d4427c24b5dbdf7d
WadaHirono <donchan.hirono528@gmail.com>
2026-05-09 00:58:16 +0900

    ボタン化デザイン適用

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -3,19 +3,56 @@ export default function ContactPage() {
     <main style={{ padding: "40px" }}>
       <h1>お問い合わせ</h1>
 
-      <div style={{ display: "flex", flexDirection: "column", gap: "16px", marginTop: "20px" }}>
-        <a href="/演奏依頼書.xlsx" download>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          gap: "20px",
+          marginTop: "30px",
+        }}
+      >
+
+        {/* 演奏依頼ボタン */}
+        <a
+          href="/演奏依頼書.xlsx"
+          download
+          style={{
+            display: "block",
+            padding: "16px",
+            background: "#000",
+            color: "#fff",
+            textAlign: "center",
+            textDecoration: "none",
+            borderRadius: "10px",
+            fontWeight: "bold",
+          }}
+        >
           🎼 演奏依頼書をダウンロード
         </a>
 
-        <a href="/レッスン申込書.xlsx" download>
+        {/* レッスン申込ボタン */}
+        <a
+          href="/レッスン申込書.xlsx"
+          download
+          style={{
+            display: "block",
+            padding: "16px",
+            background: "#000",
+            color: "#fff",
+            textAlign: "center",
+            textDecoration: "none",
+            borderRadius: "10px",
+            fontWeight: "bold",
+          }}
+        >
           🎤 レッスン申込書をダウンロード
         </a>
+
       </div>
 
-      <p style={{ marginTop: "30px" }}>
+      <p style={{ marginTop: "40px" }}>
         その他のお問い合わせはメールでご連絡ください。<br />
-        📩 donchan.hirono528@gmail.com
+        📩 example@gmail.com
       </p>
     </main>
   );
